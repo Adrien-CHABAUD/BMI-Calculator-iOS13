c12b7924679fe873a72dd3661f8574549bbf7beb
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-09-06 14:02:36 -0400

    Add a second view controller

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Adrien CHABAUD on 2022-09-06.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SeconViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

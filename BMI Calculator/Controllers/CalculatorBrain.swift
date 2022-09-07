19d056997d3d41a31dcb3b0aaf134dde4e7f10ae
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-09-07 15:10:18 -0400

    Handling nil bmi value.

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct CalculatorBrain {
     
-    var bmiValue: Float = 0.0
+    var bmiValue: Float?
+    
+    func getBMIValue() -> String {
+        return String(format: "%.1f", bmiValue ?? 0.0)
+    }
     
     mutating func calculateBMI(height: Float, weight: Float) {
         bmiValue = weight / pow(height, 2)
-    }
-    
-    func getBMIValue() -> String {
-        return String(format: "%.1f", bmiValue)
     }
 }
